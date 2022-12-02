5e10b1ebba98c57b1e3ec648d9ba5f6d4800edeb
Leonardo Lim <77972140+leonardo-lim@users.noreply.github.com>
2022-12-02 23:26:52 +0700

    Update Instagram link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ function Footer() {
         <a href="https://www.github.com/leonardo-lim" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
       </div>
       <div className="icon">
-        <a href="https://www.instagram.com/leonardolim78" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
+        <a href="https://www.instagram.com/leonardo.lim_" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
       </div>
       <button className="up-btn" id="up-btn" title="Scroll Up" onClick={scrollUp}><i className="fa fa-arrow-up"></i></button>
     </div>
